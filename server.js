1f2116fb39d3d53d3f78311a904b36e813c51e05
Javier Dominguez II <Javi7057@gmail.com>
2021-07-29 15:10:58 -0500

    created connnection for mysql

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // import express
 const express = require('express');
+const mysql = require('mysql2');
 
 // add PORT designation and app expression
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,19 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: '71reivaJ!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+)
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
