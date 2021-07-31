c479e66a90ab4f6e0499d8026b9d7acb2db69100
Javier Dominguez II <Javi7057@gmail.com>
2021-07-31 15:59:54 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // import express
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck')
 
 // add PORT designation and app expression
 const PORT = process.env.PORT || 3001;
@@ -91,6 +92,29 @@ app.delete('/api/candidate/:id', (req, res) => {
 //     }
 //     console.log(result);
 // });
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+        VALUES (?, ?, ?)`;
+        const params = [body.first_name, body.last_name, body.industry_connected];
+
+        db.query(sql, params, (err, result) => {
+            if (err) {
+                res.status(400).json({ error: err.message });
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: body
+            });
+        });
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
